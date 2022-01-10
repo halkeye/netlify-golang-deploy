c4932952af8f31c198e69432d8c80108045fce8a
Gavin Mogan <git@gavinmogan.com>
2022-01-09 22:20:30 -0800

    initial mostly working POC

---
+++ go.mod
@@ -0,0 +1,77 @@
+module github.com/halkeye/netlify-golang-deploy
+
+go 1.17
+
+require (
+	github.com/caarlos0/env/v6 v6.9.1
+	github.com/netlify/netlify-go v0.1.11
+	github.com/pkg/errors v0.9.1
+)
+
+require (
+	github.com/Azure/go-autorest/autorest v0.10.1 // indirect
+	github.com/Azure/go-autorest/autorest/adal v0.8.2 // indirect
+	github.com/Azure/go-autorest/autorest/date v0.2.0 // indirect
+	github.com/Azure/go-autorest/logger v0.1.0 // indirect
+	github.com/Azure/go-autorest/tracing v0.5.0 // indirect
+	github.com/PuerkitoBio/purell v1.1.1 // indirect
+	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
+	github.com/asaskevich/govalidator v0.0.0-20200907205600-7a23bdc65eef // indirect
+	github.com/cenkalti/backoff v2.2.1+incompatible // indirect
+	github.com/cenkalti/backoff/v4 v4.0.2 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/go-openapi/analysis v0.19.16 // indirect
+	github.com/go-openapi/errors v0.19.9 // indirect
+	github.com/go-openapi/inflect v0.19.0 // indirect
+	github.com/go-openapi/jsonpointer v0.19.5 // indirect
+	github.com/go-openapi/jsonreference v0.19.5 // indirect
+	github.com/go-openapi/loads v0.20.0 // indirect
+	github.com/go-openapi/runtime v0.19.24 // indirect
+	github.com/go-openapi/spec v0.20.0 // indirect
+	github.com/go-openapi/strfmt v0.19.11 // indirect
+	github.com/go-openapi/swag v0.19.12 // indirect
+	github.com/go-openapi/validate v0.20.0 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/go-swagger/go-swagger v0.23.0 // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/gorilla/handlers v1.4.2 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/jessevdk/go-flags v1.4.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/kr/pretty v0.2.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/kyoh86/richgo v0.3.3 // indirect
+	github.com/kyoh86/xdg v0.0.0-20171007020617-d28e4c5d7b81 // indirect
+	github.com/magiconair/properties v1.8.1 // indirect
+	github.com/mailru/easyjson v0.7.6 // indirect
+	github.com/mattn/go-isatty v0.0.0-20170925054904-a5cdd64afdee // indirect
+	github.com/mitchellh/mapstructure v1.4.0 // indirect
+	github.com/morikuni/aec v0.0.0-20170113033406-39771216ff4c // indirect
+	github.com/myitcv/gobin v0.0.14 // indirect
+	github.com/netlify/open-api v1.4.0 // indirect
+	github.com/pelletier/go-toml v1.7.0 // indirect
+	github.com/rogpeppe/go-internal v1.5.2 // indirect
+	github.com/rsc/goversion v1.2.0 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/spf13/afero v1.2.2 // indirect
+	github.com/spf13/cast v1.3.1 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.6.2 // indirect
+	github.com/subosito/gotenv v1.2.0 // indirect
+	github.com/toqueteos/webbrowser v1.2.0 // indirect
+	github.com/wacul/ptr v0.0.0-20170209030335-91632201dfc8 // indirect
+	go.mongodb.org/mongo-driver v1.4.4 // indirect
+	golang.org/x/mod v0.3.0 // indirect
+	golang.org/x/net v0.0.0-20201224014010-6772e930b67b // indirect
+	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
+	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
+	golang.org/x/text v0.3.4 // indirect
+	golang.org/x/tools v0.0.0-20200825202427-b303f430e36d // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+	gopkg.in/ini.v1 v1.54.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,243 @@
+package main
+
+import (
+	"crypto/sha1"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"sync"
+	"time"
+
+	env "github.com/caarlos0/env/v6"
+	"github.com/go-openapi/runtime"
+	openapiClient "github.com/go-openapi/runtime/client"
+	"github.com/go-openapi/strfmt"
+	"github.com/netlify/open-api/go/models"
+	netlify "github.com/netlify/open-api/go/models"
+	"github.com/netlify/open-api/go/plumbing"
+	"github.com/netlify/open-api/go/plumbing/operations"
+
+	"github.com/pkg/errors"
+)
+
+func mustGetSha1(filename string) string {
+	f, err := os.Open(filename)
+	if err != nil {
+		panic(errors.Wrap(err, "Unable to open file to sha it"))
+	}
+	defer f.Close()
+
+	hash := sha1.New()
+	if _, err := io.Copy(hash, f); err != nil {
+		panic(errors.Wrap(err, "unable to copy to sha"))
+	}
+	return fmt.Sprintf("%x", hash.Sum(nil))
+}
+
+func debug(i interface{}) {
+	jsonStr, _ := json.Marshal(i)
+	log.Printf(string(jsonStr))
+}
+
+func authInfo(netlifyAccessToken string) runtime.ClientAuthInfoWriter {
+	return runtime.ClientAuthInfoWriterFunc(func(r runtime.ClientRequest, _ strfmt.Registry) error {
+		r.SetHeaderParam("User-Agent", "User-Agent: netlifyGolangDeploy/0.0.0")
+		r.SetHeaderParam("Authorization", "Bearer "+netlifyAccessToken)
+		return nil
+	})
+}
+
+type UploadQueueAction func() error
+type config struct {
+	Token     string `env:"NETLIFY_AUTH_TOKEN"`
+	Site      string `env:"NETLIFY_SITE"`
+	Directory string `env:"NETLIFY_DIRECTORY" envDefault:"./public" envExpand:"true"`
+
+	client *plumbing.Netlify
+}
+
+type shaData struct {
+	realfilename string
+	uri          string
+}
+
+func (cfg *config) findSite(siteName string) (*netlify.Site, error) {
+	page := int32(1)
+	per_page := int32(25)
+	for {
+		// List sites
+		sites, err := netlifyClient().Operations.ListSites(
+			operations.NewListSitesParams().WithPage(&page).WithPerPage(&per_page),
+			authInfo(cfg.Token),
+		)
+		if err != nil {
+			return nil, errors.Wrap(err, "Unable to get a list of sites")
+		}
+		for _, site := range sites.GetPayload() {
+			if site.Name == siteName {
+				return site, nil
+			}
+		}
+		page = page + 1
+	}
+}
+
+func netlifyClient() *plumbing.Netlify {
+	netlifyAPIHost := "api.netlify.com"
+	netlifyAPIPath := "/api/v1"
+
+	httpClient := &http.Client{}
+
+	transport := openapiClient.NewWithClient(netlifyAPIHost, netlifyAPIPath, plumbing.DefaultSchemes, httpClient)
+	client := plumbing.New(transport, strfmt.Default)
+	return client
+}
+
+func (cfg *config) getDeploy(deployID string, wantedStatus string) (*models.Deploy, error) {
+	for {
+		deploy, err := netlifyClient().Operations.GetDeploy(
+			operations.NewGetDeployParams().WithDeployID(deployID),
+			authInfo(cfg.Token),
+		)
+		if err != nil {
+			return nil, errors.Wrap(err, "Unable to check deploy")
+		}
+		if deploy.GetPayload().State == wantedStatus {
+			// site is ready
+			return deploy.GetPayload(), nil
+		}
+
+		// site is done somehow
+		if deploy.GetPayload().State == "ready" {
+			return deploy.GetPayload(), nil
+		}
+
+		log.Printf("deployID: %s, state: %s", deployID, deploy.GetPayload().State)
+		time.Sleep(time.Duration(1) * time.Second)
+	}
+}
+
+func (cfg *config) wrapUploadJob(deployID string, realFilename string, uri string) func() error {
+	auth := authInfo(cfg.Token)
+	return func() error {
+		f, err := os.Open(realFilename)
+		if err != nil {
+			return errors.Wrap(err, "Unable to open file")
+		}
+
+		body := operations.NewUploadDeployFileParams().WithDeployID(deployID).WithPath(uri).WithFileBody(f)
+
+		_, err = netlifyClient().Operations.UploadDeployFile(body, auth)
+		return errors.Wrap(err, "Unable to upload file")
+	}
+}
+
+func main() {
+	cfg := config{}
+	if err := env.Parse(&cfg); err != nil {
+		panic(errors.Wrap(err, "Unable to parse env"))
+	}
+
+	if len(cfg.Token) == 0 {
+		panic(errors.New("Auth token is required"))
+	}
+
+	cfg.client = netlifyClient()
+
+	site, err := cfg.findSite(cfg.Site)
+	if err != nil {
+		panic(errors.Wrap(err, "Unable to find the site"))
+	}
+
+	title := "Preview Deploy"
+	filenameToSha := map[string]string{}
+	shaToFilename := map[string]*shaData{}
+	err = filepath.Walk(cfg.Directory, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			return nil
+		}
+		key, err := filepath.Rel(cfg.Directory, path)
+		if err != nil {
+			return err
+		}
+		key = "/" + key
+		filenameToSha[key] = mustGetSha1(path)
+		shaToFilename[mustGetSha1(path)] = &shaData{
+			realfilename: path,
+			uri:          key,
+		}
+
+		return nil
+	})
+	if err != nil {
+		panic(errors.Wrap(err, "Unable to walk directory"))
+	}
+	siteDeployParams := operations.NewCreateSiteDeployParams().WithSiteID(site.ID).WithTitle(&title).WithDeploy(&netlify.DeployFiles{
+		Async:  true,
+		Files:  filenameToSha,
+		Draft:  true, // FIXME
+		Branch: "preview-site-name",
+	})
+	deploy, err := cfg.client.Operations.CreateSiteDeploy(
+		siteDeployParams,
+		authInfo(cfg.Token),
+	)
+	log.Print("Finished creating deploy")
+	if err != nil {
+		panic(errors.Wrap(err, "Unable to create deploy"))
+	}
+	if deploy.GetPayload().State == "ready" {
+		log.Print("Done deploying site to " + deploy.GetPayload().DeployURL)
+	}
+
+	deployID := deploy.GetPayload().ID
+
+	preparedDeploy, err := cfg.getDeploy(deployID, "prepared")
+	if err != nil {
+		panic(errors.Wrap(err, "Unable to get deploy"))
+	}
+	log.Print("Got deploy")
+
+	queueSize := 5
+	jobChan := make(chan UploadQueueAction, queueSize)
+
+	var wg sync.WaitGroup
+	for i := 0; i < queueSize; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			// Defines a queue worker, which will execute our queue.
+			for job := range jobChan {
+				log.Printf("Processing Job")
+				job()
+			}
+		}()
+	}
+
+	for _, sha := range preparedDeploy.Required {
+		log.Printf("Enqueuing Job")
+		// Append job to jobs slice.
+		jobChan <- cfg.wrapUploadJob(deployID, shaToFilename[sha].realfilename, shaToFilename[sha].uri)
+	}
+
+	close(jobChan)
+
+	log.Print("Done enqueuing jobs")
+	wg.Wait()
+	log.Print("Done uploading")
+
+	_, err = cfg.getDeploy(deployID, "ready")
+
+	if err != nil {
+		panic(errors.Wrap(err, "finish deployment"))
+	}
+
+	log.Print("Done deploying site to " + deploy.GetPayload().DeployURL)
+}
